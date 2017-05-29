5f521a8829e36ea0230d5bc501919414a3bf666d
Tatiana Freire Sosa <tatifreiresosa@gmail.com>
2017-05-29 12:33:04 -0300

    Primer commit
    primer commit

---
+++ colaboradores2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Colaboradores </title>
+	<?php include('estilos.php') ?>
+</head>
+<body>
+	<?php include 'header2.php'; ?>
+
+	<?php include 'footer2.php'; ?>
+
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio- Meraki</title>
+  <?php include('estilos.php') ?>
+  
+</head>
+<body>
+
+	<?php include 'header2.php'; ?>
+
+	<div class="container">
+    <div class="row Imagen principal">
+      <div class="col-sm-offset-3"></div>
+      <div align="center" class="col-sm-6"><img class="img-responsive" class="frase" src="grafica/frase.png" alt="" style="margin: 10px;"></div>
+      <div class="col-sm-offset-3"></div>
+    </div>
+    <div class="row Fotitos">
+      <div class="col-md-offset-2"></div>
+      <div class="col-md-3">
+        
+      </div>
+      <div class="col-md-offset-2"></div>
+      <div class="col-md-3">
+        
+      </div>
+      <div class="col-md-offset-2"></div>
+    </div>
+    <div class="row ">
+        <div class="col-md-offset-3"></div>
+        
+        <div class="col-md-offset-3"></div>
+    </div>
+    <div class="row Comunidad"> 
+        <div class="col-md-offset-3"></div>
+        <div class="col-md-6"></div>
+        <div class="col-md-offset-3"></div>
+    </div>
+</div>
+
+
+<?php include 'footer2.php'; ?>
+
+</body>
+</html>
