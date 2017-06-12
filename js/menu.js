961bac90225c95fa039d018055daeb81f8eb25e8
Tatiana Freire Sosa <tatifreiresosa@gmail.com>
2017-06-12 18:02:35 -0300

    arreglo

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,5 @@
-window.onload = funciones;
+$(".fa-bars").click(function(){
+    $(".menu-oculto").toggle("slow", function(){
 
-function funciones(){
-	var hamburguesa = document.querySelector(".fa-bars");
-	hamburguesa.addEventListener("click", mostrar);
-	function mostrar(){
-		if(document.querySelector(".menu-oculto").hasAttribute("style", "display:none") === true){
-			document.querySelector(".menu-oculto").removeAttribute("style", "display:none");
-			document.querySelector(".menu-oculto").setAttribute("style", "display:inline-block");
-		};	
-	};
-
-
-	hamburguesa.addEventListener("click", ocultar);
-	function ocultar(){
-		if (document.querySelector(".menu-oculto").hasAttribute("style", "display:inline-block") === true){
-			document.querySelector(".menu-oculto").removeAttribute("style", "display:inline-block");
-			document.querySelector(".menu-oculto").setAttribute("style", "display:none");
-		};
-	};
-}
+});
+});
